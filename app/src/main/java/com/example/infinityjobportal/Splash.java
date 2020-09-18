e28ba77642f66c9f9183fe008765a8cb78a05fad
ajitdhdharia <ajitdhdhariaaa@gmail.com>
2020-09-17 21:07:50 -0400

    Nothing.

--- app/src/main/java/com/example/infinityjobportal/Splash.java
+++ app/src/main/java/com/example/infinityjobportal/Splash.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
-
+    private static final String TAG = "Splash";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
