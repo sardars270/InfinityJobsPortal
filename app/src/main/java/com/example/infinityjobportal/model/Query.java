a8407974b68d8a352bed03ab91c4731266c50357
Sarkariamanpreet <52627103+Sarkariamanpreet@users.noreply.github.com>
2020-10-10 09:43:27 -0400

    implementation of Query Section from Client is Completed.

--- app/src/main/java/com/example/infinityjobportal/model/Query.java
+++ app/src/main/java/com/example/infinityjobportal/model/Query.java
@@ -1,5 +1,56 @@
 package com.example.infinityjobportal.model;
 
-public class Query {
+import com.google.firebase.firestore.Exclude;
 
+import java.io.Serializable;
+
+
+public class Query implements Serializable {
+    @Exclude
+    private String id;
+    private String feedbackQuery;
+    private String editSubject;
+    private String userid;
+
+    public Query(  String editSubject,String feedbackQuery,String userid) {
+        this.editSubject=editSubject;
+        this.feedbackQuery = feedbackQuery;
+        this.userid=userid;
+    }
+
+
+    public Query() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getEditSubject()
+    {
+        return editSubject;
+    }
+    public void setEditSubject(String editSubject)
+    {
+        this.editSubject=editSubject;
+    }
+
+    public String getFeedbackQuery() {
+        return feedbackQuery;
+    }
+
+    public void setFeedbackQuery(String feedbackQuery) {
+        this.feedbackQuery = feedbackQuery;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
 }
