a0387752e347052f8c16bb5a3398b8dc5845a664
varun <varunmahajan292@gmail.com>
2020-10-24 20:54:20 -0700

    layouts

---
+++ app/src/main/java/com/example/infinityjobportal/filters_activity.java
@@ -0,0 +1,13 @@
+package com.example.infinityjobportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class filters_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters_activity);
+    }
+}
