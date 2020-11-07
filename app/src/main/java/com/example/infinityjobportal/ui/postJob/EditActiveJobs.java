b84b6792343b3aca5a2ca1ab67ef1dce419952c2
ajitdhdharia <ajitdhdhariaaa@gmail.com>
2020-11-07 13:12:31 -0500

    Active Jobs Edit

---
+++ app/src/main/java/com/example/infinityjobportal/ui/postJob/EditActiveJobs.java
@@ -0,0 +1,18 @@
+package com.example.infinityjobportal.ui.postJob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.infinityjobportal.R;
+
+public class EditActiveJobs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_active_jobs);
+
+
+    }
+}
